1830fe256d84603a9ddbb15e4082229981641a5e
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-01-13 10:26:17 +0000

    Completed animal challenge

---
+++ src/com/qa/main/Animal.java
@@ -0,0 +1,12 @@
+package com.qa.main;
+
+public abstract class Animal {
+
+    protected String colour;
+    protected int numberOfEyes;
+    protected boolean hasClaws;
+
+    public abstract void breath();
+    public abstract void useMitochondria();
+
+}
